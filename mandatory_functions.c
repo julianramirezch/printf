ddeff9ce6e0f1701af66f2cd66e1d3c70e0d6fc1
julianramirezch <julianramirezch1@gmail.com>
2020-03-14 19:30:36 +0000

    [add]: mandatory_functions.c

---
+++ mandatory_functions.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+
+void print_char(va_list arguments, char *buffer, unsigned int *position)
+{
+	buffer[*position] = va_arg(arguments, int);
+	*position += 1;
+}
+
+void print_str(va_list arguments, char *buffer, unsigned int *position)
+{
+	char *str = va_arg(arguments, *char);
+	int cont = 0;
+	while (str)
+	{
+		buffer[*position] = str[cont];
+		*position++;
+		cont++;
+	}
+}
+
+void print_int(va_list arguments, char *buffer, unsigned int *position)
+{
+	int j = va_arg(arguments, int);
+	int cont = 0;
+	int copycont;
+	int copyj= j
+	
+	while((copyj/10) > )
+	{
+		cont++;
+		copyj = copyj / 10;
+	}
+	cont++;
+	copycont = cont;
+	while (cont > 0)
+	{
+		buffer[*position + cont - 1] = (copyj % 10) + 48;
+		cont--;
+		copyj = copyj / 10;
+	}
+	*position += copycont;
+
+void print_percen(va_list arguments, char *buffer, unsigned int *position)
+{
+	buffer[*position] = '%';
+	*position++;
+}
+/*void print_decimal(va_list arguments, char *buffer, unsigned int *position)
+{
+	return;
+}
+*/
