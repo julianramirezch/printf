0ee972b424e6df25261e2c77328a7e30fb1e88e4
julianramirezch <julianramirezch1@gmail.com>
2020-03-16 21:30:40 +0000

    [fix]; Style and add *len

--- numbers_functions.c
+++ numbers_functions.c
@@ -1,47 +1,51 @@
 #include "holberton.h"
 
-int itoaa(va_list arguments, char* buffer, unsigned int *position)
+/**
+  * itoaa - Integer to Char.
+  * @argum: va_list
+  * @buffer: buffer.
+  * @len: Lenght
+  * @pos: Position.
+  * Return: Pointer.
+  */
+int itoaa(va_list argum, char *buffer, int *pos, int *len __attribute__((unused)))
 {
 	int numbers = 0, negative = 0, count = 0;
-	int n = va_arg(arguments, int);
+	int n = va_arg(argum, int);
 
 	numbers = abs(n);
 
 	if (numbers == 0)
 	{
-		buffer[*position] = '0';
-		*position += 1;
+		buffer[*pos] = '0';
+		*pos += 1;
 	}
-
-
-
 	while (numbers)
 	{
 		int r = numbers % 10;
 
 		if (r >= 10)
 		{
-			buffer[*position] = 65 + (r - 10);
-			*position += 1;
+			buffer[*pos] = 65 + (r - 10);
+			*pos += 1;
 			count++;
 		}
 		else
 		{
-			buffer[*position] = 48 + r;
-			*position += 1;
+			buffer[*pos] = 48 + r;
+			*pos += 1;
 			count++;
 		}
 		numbers /= 10;
 	}
 
-	if (n < 0 )
+	if (n < 0)
 	{
-		buffer[*position] = '-';
-		*position += 1;
+		buffer[*pos] = '-';
+		*pos += 1;
 		count++;
 		negative = 1;
 	}
-	reverse(buffer, position, count, negative);
-
-	return(*position);
+	reverse(buffer, pos, count, negative);
+	return (*pos);
 }
