2cb94c383b40b58c24474cbb30af4ed1c672a0fe
andres <andressantiagore@gmail.com>
2020-03-16 00:29:06 +0000

    [feature]: change the whole body, built the main function

--- _printf.c
+++ _printf.c
@@ -4,22 +4,25 @@
  * @format: the recieve format.
  * Return: Pointer
  */
+int (*get_function(char *c, va_list arguments, char *buffer, unsigned int *position))(va_list, char *, unsigned int *);
+
+
 int _printf(const char *format, ...)
 {
+	va_list arguments;
+	char *buffer = NULL;
+        int cfor = 0, positionBuffer = 0;
+	unsigned int *position = NULL;
+	char *valor = NULL;
 /*Validacion si para ver si hay un formato*/
 	if (format == NULL)
 		return (0);
-
-	va_list arguments;
 /* Asignando un puntero con un buffer principal */
-	char *buffer = malloc (2048);
+	buffer = malloc(2048);
 	if (buffer == NULL)
 		return (0);
 
-	char valor = NULL;
-	int cfor = 0, carg = 0, positionBuffer = 0;
-
-	int *position = malloc(sizeof (int));
+	position = malloc(sizeof (int));
 	if (position == NULL)
 		return (0);
 
@@ -51,4 +54,31 @@ int _printf(const char *format, ...)
 	va_end(arguments);
 
 	return (positionBuffer);
+}
+
+int (*get_function(char *c, va_list arguments, char *buffer, unsigned int *position))(va_list, char *, unsigned int *)
+{
+	int carg = 0;
+
+	cases print_format[] = {
+		{'c', print_char},
+		{'s', print_str},
+		{'i', print_int},
+		{'%', print_percen},
+		{'d', print_decimal},
+		{0, NULL},
+	};
+
+	while (print_format[carg].arg)
+	{
+/* La condicion va aumentar hasta que llegue un argumento nulo */
+		if (*c == print_format[carg].arg)
+		{
+			print_format[carg].f(arguments,buffer, position);
+			break;
+		}
+		carg++;
+	}
+
+	return (0);
 }
